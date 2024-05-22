70840d94f1ce87a464bf3756f338f4e7ab5d5681
PashkaGold <144147129+PashkaGold@users.noreply.github.com>
2024-05-22 21:10:09 +0300

    Mob , Mob Atak Management

--- Моя гра спроба 3/Assets/Script/Mob 1.cs
+++ Моя гра спроба 3/Assets/Script/Mob 1.cs
@@ -7,7 +7,7 @@ public class Mob1 : MonoBehaviour
     public float speed;
 
     private float waitTime;
-    public float startwaitTime;
+    public float startWaitTime;
 
     public Transform[] moveSpots;
     private int randomSpot;
@@ -15,21 +15,66 @@ public class Mob1 : MonoBehaviour
     private void Start()
     {
         randomSpot = Random.Range(0, moveSpots.Length);
-        waitTime = startwaitTime;
-
+        waitTime = startWaitTime;
     }
+
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        MoveTowardsSpot();
+        CheckIfReachedSpot();
+    }
+
+    private void MoveTowardsSpot()
+    {
+        if (IsPathBlocked())
+        {
+            FindNewSpot();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        }
+    }
+
+    private void CheckIfReachedSpot()
+    {
         if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
         {
             if (waitTime <= 0)
             {
-                randomSpot = Random.Range(0, moveSpots.Length);
-                waitTime = startwaitTime;
+                FindNewSpot();
+                waitTime = startWaitTime;
             }
             else
+            {
                 waitTime -= Time.deltaTime;
+            }
         }
+    }
+
+    private void FindNewSpot()
+    {
+        int attempts = 0;
+        int maxAttempts = 10; // ћаксимальна к≥льк≥сть спроб знайти нову точку
+
+        do
+        {
+            randomSpot = Random.Range(0, moveSpots.Length);
+            attempts++;
+        }
+        while (IsPathBlocked() && attempts < maxAttempts);
+    }
+
+    private bool IsPathBlocked()
+    {
+        Vector2 direction = moveSpots[randomSpot].position - transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, direction.magnitude);
+
+        if (hit.collider != null && hit.collider.gameObject != gameObject)
+        {
+            // якщо Ї коллайдер, ≥ це не сам моб, то шл€х заблокований
+            return true;
+        }
+        return false;
     }
 }
