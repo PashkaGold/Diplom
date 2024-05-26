683e0e25b98aa1422617650239f0eac5de3895b1
PashkaGold <144147129+PashkaGold@users.noreply.github.com>
2024-05-26 20:58:20 +0300

    Mob Map

--- Моя гра спроба 3/Assets/Script/Spawn.cs
+++ Моя гра спроба 3/Assets/Script/Spawn.cs
@@ -9,21 +9,31 @@ public class Spawn : MonoBehaviour
     Vector2 whereToSpawn;
     public float spavnDelay;
     float nextSpawn = 0.0f;
+
+    // Додано для визначення розміру області спавну
+    public float spawnAreaWidth = 16f;
+    public float spawnAreaHeight = 1f;
+
     void Start()
     {
-        
+
     }
 
-    
     void Update()
     {
-        if(Time.time > nextSpawn) 
+        if (Time.time > nextSpawn)
         {
             nextSpawn = Time.time + spavnDelay;
-            randomX = Random.Range(-8, 8);
-            whereToSpawn = new Vector2 (randomX, transform.position.y);
-            GameObject Enemi = Instantiate (obj, whereToSpawn,Quaternion.identity);
-            
+            randomX = Random.Range(-spawnAreaWidth / 2, spawnAreaWidth / 2);
+            whereToSpawn = new Vector2(randomX, transform.position.y);
+            GameObject Enemi = Instantiate(obj, whereToSpawn, Quaternion.identity);
         }
+    }
+
+    // Метод для візуалізації області спавну
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green; // Колір області спавну
+        Gizmos.DrawWireCube(transform.position, new Vector3(spawnAreaWidth, spawnAreaHeight, 1));
     }
 }
